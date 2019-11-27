83a3bc338eb5a3a6dd9b9d63716636bbdc921582
Andy Turner <a.g.d.turner@leeds.ac.uk>
2019-11-27 14:07:36 +0000

    Added test for W1*.

--- src/main/java/uk/ac/leeds/ccg/andyt/ukpc/UKPC_Checker.java
+++ src/main/java/uk/ac/leeds/ccg/andyt/ukpc/UKPC_Checker.java
@@ -24,7 +24,7 @@ package uk.ac.leeds.ccg.andyt.ukpc;
  * contains methods for efficiently check if Strings are of the right length and
  * have the digits 0 to 9 and restricted sets of alphabetic characters in viable
  * places.
- * 
+ *
  * The general format of a postcode is given in:
  * https://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom
  *
@@ -40,7 +40,7 @@ public class UKPC_Checker {
     public static final int TYPE_AAN = 5;
     public static final int TYPE_AAA = 6;
     public static final int TYPE_AN = 7;
-     
+
     public static final char[] AtoZ = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
     public static final char[] AtoZ_not_CIKMOV = "ABDEFGHJKLNPQRSTUWXYZ".toCharArray();
     public static final char[] AtoZ_not_QVX = "ABCDEFGHIJKLMNOPRSTUWYZ".toCharArray();
@@ -147,6 +147,12 @@ public class UKPC_Checker {
             char[] fpp = s0.substring(0, length - 3).toCharArray();
             int fppt = getFirstPartPostcodeType(fpp);
             return fppt != 0;
+        } else {
+            if (isValidSecondPartUnitPostcodeOld(s0.substring(length - 3, length).toCharArray())) {
+                char[] fpp = s0.substring(0, length - 3).toCharArray();
+                int fppt = getFirstPartPostcodeType(fpp);
+                return fppt != 0;
+            }
         }
         return false;
     }
@@ -225,6 +231,24 @@ public class UKPC_Checker {
     }
 
     /**
+     * @param spp the second part of the postcode to test if it is a valid
+     * second part of a postcode for a unit postcode.
+     * @return true if spp is of length 3 and where: spp[0] is in
+     * {@link #digits}; spp[1] is in {@link #AtoZ_not_CIKMOV}; and, spp[1] is in
+     * {@link #AtoZ_not_CIKMOV}.
+     */
+    public boolean isValidSecondPartUnitPostcodeOld(char[] spp) {
+        if (spp.length == 3) {
+            if (contains(spp[0], digits)
+                    && contains(spp[1], AtoZ)
+                    && contains(spp[2], AtoZ)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
      * @param fpp First Part of a Postcode.
      * @return Where A stands for an alphabetical character and N stands for
      * digit: 0 if fpp is not a valid first part for a postcode; 1 if it is of
@@ -258,19 +282,44 @@ public class UKPC_Checker {
                         return TYPE_ANN;
                     } else if (contains(fpp[2], ABCDEFGHJKSTUW)) {
                         return TYPE_ANA;
+                    } else if (fpp[2] == 'P') {
+                        // Special case N1P
+                        return TYPE_ANA;
+                    } else if (fpp[0] == 'W') {
+                        if (contains(fpp[2], AtoZ)) {
+                            // Special case W1N
+                            // Special case W1R
+                            // Special case W1V
+                            // Special case W1X
+                            // Special case W1Y
+                            //System.out.println(String.valueOf(fpp));
+                            return TYPE_ANA;
+                        } else {
+                            return 0;
+                        }
+                    } else if (fpp[2] == 'N') {
+                        return TYPE_ANA;
                     } else {
                         return 0;
                     }
                 } else if (contains(fpp[1], AtoZ_not_IJZ)) {
                     if (contains(fpp[2], digits)) {
                         return TYPE_AAN;
+                    } else if (fpp[1] == 'P' && fpp[2] == 'T') {
+                        // NPT.
+                        return TYPE_AAA;
+                    } else if (fpp[1] == 'R' && fpp[2] == 'O') {
+                        // CRO.
+                        return TYPE_AAA;
                     } else {
                         return 0;
                     }
                 } else {
                     if (fpp[1] == 'I' && fpp[2] == 'R') {
-                        // GIR only.
+                        // GIR.
                         return TYPE_AAA;
+                    } else {
+                        return 0;
                     }
                 }
             } else {
